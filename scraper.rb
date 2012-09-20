6aee1cfbea45005055ad02e356b41c52dd9b2c0c
Al Johri <al.johri@gmail.com>
2012-09-20 16:52:48 -0500

    updated scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -58,6 +58,9 @@ gsub(/<\/?[^>]*>/, "")
 
 require 'net/http'
 require 'rexml/document'
+
+$output
+
 urlBase = 'http://maps.google.com/maps/ms?msa=0&msid='
 uuidLee = '209440134627172181780' # Lee
 uuidJess = '200725481515120343586' # Jessica
@@ -77,20 +80,96 @@ routes = 	[['209440134627172181780', '00046246a50cf04a21430'],  # Lee , Shop-N-R
 			 ['200725481515120343586', '0004b645f2cbd1b31ad1f']]  # Jes , Frostbite Sheridan
 
 
+
 for rt in routes
 
 	url = urlBase + rt[0] + "." + rt[1] + format
-
 	xml_data = Net::HTTP.get_response(URI.parse(url)).body
 	doc = REXML::Document.new(xml_data)
 	root = doc.root
-
 	name = root.elements[1].elements['name'].text
-	#description = root.elements[1].elements['description'].text.gsub(/\n/," ")
-	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| "     " + x.elements['name'].text + ": " + x.elements['description'].text.scan(/([0-9]+:[0-9]+[ap]?[*]?)(,|\s)?/).map { |i| i[0].to_s }.join(", ")}
-	#placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text }
+	description = root.elements[1].elements['description'].text.gsub(/\n/," ")
+
+	# Special Cases
+
+	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text }
+
+	# if name.include? "Shop-N-Ride"
+	# 	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text }
+	# elsif name.include? "Intercampus Shuttle"
+	# 	#placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text }
+	# 	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text}
+	# elsif name.include? "Chicago Express"
+	# 	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text }
+	# elsif name.include? "Campus Loop Shuttle"
+	# 	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text.scan(/(:[0-9]+)\s+(:[0-9]+)/).map { |i| i[0].to_s + " | " + i[1].to_s }.join(", ")}
+	# elsif name.include? "Frostbite" # Frostbite Express and Sheridan
+	# 	# Adjust for AM/PM and am/pm instead of a/p
+	# 	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text.scan(/([0-9]+:[0-9]+\s+(AM|PM)?[*]?)(,|\s)?/i).map { |i| i[0].to_s }.join(", ")}
+	# else
+	# 	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text.scan(/([0-9]+:[0-9]+[ap]?[*]?)(,|\s)?/).map { |i| i[0].to_s }.join(", ")}
+	# end
+
+
+	puts "Name: " + name
+	puts "Description: " + description
+	puts "Placemarks: "
+	puts placemarks
+	puts ""
+	puts ""
+
+	url = nil
+	xml_data = nil
+	doc = nil
+	root = nil
+	name = nil
+	description = nil
+	placemarks = nil
+end
 
 =begin
+class A
+  def initialize(string, number)
+    @string = string
+    @number = number
+  end
+ 
+  def to_s
+    "In A:\n   #{@string}, #{@number}\n"
+  end
+ 
+  def to_json(*a)
+    {
+      "json_class"   => self.class.name,
+      "data"         => {"string" => @string, "number" => @number }
+    }.to_json(*a)
+  end
+ 
+  def self.json_create(o)
+    new(o["data"]["string"], o["data"]["number"])
+  end
+end
+
+a = A.new("hello world", 5)
+json_string = a.to_json
+puts json_string
+puts JSON.parse(json_string)
+
+=end
+
+
+
+#placemarks = REXML::XPath.match(root, "//Placemark[Point]").map {|x| x.elements['name'].text + ": " + x.elements['description'].text }
+
+
+
+=begin
+
+get '/' do
+	"hello world"
+end
+
+
 	stops = Hash.new
 	times = []
 	placemarks = REXML::XPath.match(root, "//Placemark[Point]").map { 
@@ -101,12 +180,3 @@ for rt in routes
 		puts stops
 	}
 =end
-	puts "Name: " + name
-	#puts "Description: " + description
-	puts "Placemarks: "
-	puts placemarks
-	puts ""
-	puts ""
-
-end
-
